cc715f73ebeb52cf3d274a254d5b3533ecccfac4
Pratap <spmaurya106@gmail.com>
2024-05-19 22:20:39 -0500

    madir image

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 window.onload = function() {
     document.getElementById("my_audio").play();
-=======
-window.onload = function() {
-    document.getElementById("my_audio").play();
->>>>>>> f9510a4ae30ad52cf22d87bab40156b7cf5ff8b5
 };
